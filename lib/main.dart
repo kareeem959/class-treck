1878c8a54cfba708e1b0967daac92d18391c362c
kareem <kareemabdalatef959@gmail.com>
2026-09-24 18:04:35 +0200

    student page

--- lib/main.dart
+++ lib/main.dart
@@ -7,8 +7,7 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
-  @override
+   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
@@ -25,7 +24,7 @@ class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
   // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
+  // that it has agfhfgghfghfghfyhrtyh State object (defined below) that contains fields that affect
   // how it looks.
 
   // This class is the configuration for the state. It holds the values (in this
